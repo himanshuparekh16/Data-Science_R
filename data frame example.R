1c103966d03f7583cc2e7146cde33c1a18b3fc42
himanshuparekh16 <70227082+himanshuparekh16@users.noreply.github.com>
2020-08-26 20:48:32 +0530

    Add files via upload

---
+++ data frame example.R
@@ -0,0 +1,12 @@
+df <- data.frame(x = 1:3 , y = c('a','b','c'))
+df
+
+df1 <- data.frame(x = 1:3 , y = c('a','b','c') , z = 1:3)
+df1
+
+df[1]
+df[0,1]
+df[1,1] ; df[3,1]
+
+df[c(1,3),c(1,2)]
+
---
+++ example.R
@@ -0,0 +1,62 @@
+1+1
+
+2+3*4
+
+3^2
+
+exp(1)
+
+sqrt(10)
+
+pi
+
+2 * pi * 6378
+
+6 * (3+2)/5
+
+(6 * 3) + 5/10
+
+ 
+#Create a vector
+
+x <- c(12, 15, 20, 100)
+
+
+#Addition of vector
+
+vect <- c(2,0,0,4)
+
+vect1 <- c(1,2,3,4)
+
+vect + vect1
+
+
+
+#Multiplication of vector
+
+vect1 * 10
+
+
+
+
+#Accessing the elements of the vector
+
+vect[1]
+
+
+#Replacing elements of vector
+
+vect[2] <- 3
+vect
+
+
+#Character elements in vector
+
+char <- c('abc', 'def', 'ghi', 'jkl')
+
+
+#Characters and numbers in one vector
+
+char_num <- c('a','b','c',1,2,3,'%')
+char_num
+
